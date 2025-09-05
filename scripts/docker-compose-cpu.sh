a5a85648ba218f63a7a36789774fcfbaba7317df
rwiv <zc798546@gmail.com>
2025-09-05 23:22:47 +0900

    build: update docker compose

--- scripts/docker-compose-cpu.sh
+++ scripts/docker-compose-cpu.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-sudo docker compose -f ./docker/docker-compose-batch-cpu.yml --env-file ./secret/.env stop
-sudo docker compose -f ./docker/docker-compose-batch-cpu.yml --env-file ./secret/.env rm -f
+sudo docker compose -f ./docker/docker-compose-cpu.yml --env-file ./secret/.env stop
+sudo docker compose -f ./docker/docker-compose-cpu.yml --env-file ./secret/.env rm -f
 
-sudo docker compose -f ./docker/docker-compose-batch-cpu.yml --env-file ./secret/.env up -d
+sudo docker compose -f ./docker/docker-compose-cpu.yml --env-file ./secret/.env up -d
--- scripts/docker-compose-gpu.sh
+++ scripts/docker-compose-gpu.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-sudo docker compose -f ./docker/docker-compose-batch-gpu.yml --env-file ./secret/.env stop
-sudo docker compose -f ./docker/docker-compose-batch-gpu.yml --env-file ./secret/.env rm -f
+sudo docker compose -f ./docker/docker-compose-gpu.yml --env-file ./secret/.env stop
+sudo docker compose -f ./docker/docker-compose-gpu.yml --env-file ./secret/.env rm -f
 
-sudo docker compose -f ./docker/docker-compose-batch-gpu.yml --env-file ./secret/.env up -d
+sudo docker compose -f ./docker/docker-compose-gpu.yml --env-file ./secret/.env up -d
