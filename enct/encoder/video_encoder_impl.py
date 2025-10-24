81c8450f817ee2fea4802708fc49d5abd9c935f3
rwiv <zc798546@gmail.com>
2025-10-25 06:19:16 +0900

    feat: add EncodingQualityEstimator

---
+++ enct/encoder/video_encoder_impl.py
@@ -0,0 +1,72 @@
+import asyncio
+import subprocess
+import sys
+
+from aiofiles import os as aios
+from pyutils import log, exec_process, cur_duration, check_returncode
+
+from .command_resolver import FfmpegCommandResolver
+from .encoding_request import EncodingRequest
+from .output_filter import FfmpegOutputFilter
+from .progress_parser import FfmpegEncodingProgressParser
+from .video_encoder import EncodingResult, VideoEncoder
+
+
+class VideoEncoderImpl(VideoEncoder):
+    def __init__(self):
+        self.__out_filter = FfmpegOutputFilter()
+        self.__cmd_resolver = FfmpegCommandResolver()
+        self.__parser = FfmpegEncodingProgressParser()
+
+    async def encode(self, req: EncodingRequest, logging: bool = False) -> EncodingResult:
+        if await aios.path.exists(req.out_file_path):
+            raise FileExistsError(f"Output file {req.out_file_path} already exists.")
+
+        log.info("Starting encoding", req.to_log_attr())
+        start_time = asyncio.get_event_loop().time()
+
+        command = self.__cmd_resolver.resolve(req)
+        process = await exec_process(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        assert process.stdout is not None
+
+        stream_q_sum = 0.0
+        stream_q_cnt = 0
+        bitrate_sum = 0.0
+        bitrate_cnt = 0
+        speed_sum = 0.0
+        speed_cnt = 0
+
+        while True:
+            chunk = await process.stdout.read(sys.maxsize)
+            if not chunk:
+                break
+            info = self.__parser.parse(chunk)
+            if info.stream_q is not None:
+                stream_q_sum += info.stream_q
+                stream_q_cnt += 1
+            if info.bitrate_kbits is not None:
+                bitrate_sum += info.bitrate_kbits
+                bitrate_cnt += 1
+            if info.speed is not None:
+                speed_sum += info.speed
+                speed_cnt += 1
+            if logging:
+                log.debug("Encoding Progress", info.model_dump(mode="json"))
+
+        stdout_raw, stderr_raw = await process.communicate()
+        check_returncode(process, command, stdout_raw, stderr_raw)
+
+        stderr = self.__out_filter.filtered_stderr(stderr_raw)
+        if stderr is not None and len(stderr.filtered) > 0:
+            await aios.remove(req.out_file_path)
+            log.error("Encoding failed with error", {"err": stderr.filtered})
+            raise RuntimeError("Encoding failed with error")
+
+        return EncodingResult(
+            output_file=req.out_file_path,
+            quantizer_avg=stream_q_sum / stream_q_cnt if stream_q_cnt > 0 else None,
+            bitrate_avg=bitrate_sum / bitrate_cnt if bitrate_cnt > 0 else None,
+            speed_avg=speed_sum / speed_cnt if speed_cnt > 0 else None,
+            duration=cur_duration(start_time),
+            stderr=stderr,
+        )
