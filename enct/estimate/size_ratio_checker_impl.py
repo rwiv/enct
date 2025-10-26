fbce00448cb58d2c23382ba9bca4ea738f483a14
rwiv <zc798546@gmail.com>
2025-10-27 06:09:41 +0900

    feat: update SizeRatioCheckerImpl

--- enct/estimate/size_ratio_checker_impl.py
+++ enct/estimate/size_ratio_checker_impl.py
@@ -22,7 +22,10 @@ class SizeRatioCheckerImpl(SizeRatioChecker):
             raise ValueError(f"File without extension: {enc_req.src_file_path}")
 
         ratio_sum = 0
-        for start, end in divide_time_range("0", vid_info.duration, ck_req.n_parts):
+        for i, (start, end) in enumerate(divide_time_range("0", vid_info.duration, ck_req.n_parts + 1)):
+            if i == 0:
+                continue
+
             src_req = enc_req.to_copy_req()
             src_req.out_file_path = path_join(self.__tmp_dir_path, f"{uuid.uuid4()}.{ext}")
             src_req.opts.time_range = get_sub_time_range(start, end, ck_req.enc_duration)
